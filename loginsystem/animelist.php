857cc5e999b945533cd3711ad8e94c077800513b
Redwan12345678 <redwanur.rafi@northsouth.edu>
2021-05-25 02:53:38 +0600

    First commit

---
+++ loginsystem/animelist.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link rel="stylesheet" type="text/css" href="css/stylewelcome.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#"><img src="images/anime.png" alt=""></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="welcome.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="animelist.php">Anime List</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="animenews.php">Anime News</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="faq.php">FAQ</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="logout.php">Log out</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="list-group">
+  <a href="#" class="list-group-item list-group-item-action">Pick what youlike and make to tell us what you think</a>
+
+  
+  <a href="#" class="list-group-item list-group-item-action list-group-item-primary">Full Metal Alchamist</a>
+  <a href="#" class="list-group-item list-group-item-action list-group-item-secondary">One Piece</a>
+  <a href="#" class="list-group-item list-group-item-action list-group-item-success">Tokyo Revengers</a>
+  <a href="#" class="list-group-item list-group-item-action list-group-item-danger">Sword Art Online</a>
+  <a href="#" class="list-group-item list-group-item-action list-group-item-warning">The time I got reincaranted as a Slime</a>
+  <a href="#" class="list-group-item list-group-item-action list-group-item-info">Van Helsing</a>
+  <a href="#" class="list-group-item list-group-item-action list-group-item-light">Tokyo Ghouls</a>
+  <a href="#" class="list-group-item list-group-item-action list-group-item-dark">Naruto</a>
+</div>
---
+++ loginsystem/animenews.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link rel="stylesheet" type="text/css" href="css/stylewelcome.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#"><img src="images/anime.png" alt=""></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="welcome.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="animelist.php">Anime List</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="animenews.php">Anime News</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="faq.php">FAQ</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="logout.php">Log out</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="list-group">
+  <a href="#" class="list-group-item list-group-item-action flex-column align-items-start active">
+    <div class="d-flex w-100 justify-content-between">
+      <h5 class="mb-1">Toonami Airs Black Clover Anime's 4th Season on June 19</h5>
+      <small>3 days ago</small>
+    </div>
+    <p class="mb-1">The official Facebook page for Adult Swim's Toonami programming block confirmed on Monday that the Black Clover television anime will continue airing on the block for its fourth season, starting on June 19 at 2:00 a.m. EDT. The series' third season is currently airing on Toonami.</p>
+    <small>Source: Toonami's Facebook page</small>
+  </a>
+  <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+    <div class="d-flex w-100 justify-content-between">
+      <h5 class="mb-1">Sega to Release Demon Slayer -Kimetsu no Yaiba- The Hinokami Chronicles Console Game in English in Asia This Year</h5>
+      <small class="text-muted">3 days ago</small>
+    </div>
+    <p class="mb-1">Sega opened an English website on Monday for the Kimetsu no Yaiba: Hinokami Keppūtan game, revealing that the game will launch in English as Demon Slayer: Kimetsu no Yaiba- The Hinokami Chronicles in Asia this year for PlayStation 4, PlayStation 5, Xbox One, Xbox Series X and S, and PC via Steam. The company streamed a trailer and development report</p>
+    <small class="text-muted">Source: Demon Slayer: Kimetsu no Yaiba- The Hinokami Chronicles game's website via Gematsu</small>
+  </a>
+  <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+    <div class="d-flex w-100 justify-content-between">
+      <h5 class="mb-1">Demon Slayer Film Earns US$43.96 Million in U.S., Godzilla vs. Kong Film Earns US$96.91 Million</h5>
+      <small class="text-muted">3 days ago</small>
+    </div>
+    <p class="mb-1">Box Office Mojo reported on Monday that the Demon Slayer – Kimetsu no Yaiba – The Movie: Mugen Train film earned an estimated US$1,311,513 at the United States box office last weekend, and has now earned an estimated US$43,956,487. The film ranked at #6 in its fifth weekend. Additionally, Box Office Mojo reported that Godzilla vs. Kong earned an estimated US$1,435,000 in its eighth weekend in the United States to rank at #5. The film has earned an estimated US$96,910,216 in the United States</p>
+    <small class="text-muted">Source: Box Office Mojo</small>
+  </a>
+</div>
